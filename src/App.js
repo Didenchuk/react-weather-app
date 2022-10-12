112f89a82a1a834e62478a21b851a2e269ee8d59
Didenchuk <tdidenchuk@gmail.com>
2022-10-12 19:37:19 +0200

    Added the GitHup repo Url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather App
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded by Tanya Didenchuk and is {" "}
+    <a href="https://github.com/Didenchuk/react-weather-app" target="_blank">
+    open-sourced on GitHub
+    </a>
+    </footer>
     </div>
   );
 }
