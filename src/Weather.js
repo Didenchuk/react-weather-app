74731cb7155a8a921b300a746c19c71f4d53200f
Didenchuk <tdidenchuk@gmail.com>
2022-10-23 18:40:02 +0200

    Added weatherIcon component

--- src/Weather.js
+++ src/Weather.js
@@ -20,7 +20,7 @@ setWeathwerData({
     date: new Date(response.data.dt * 10000),
     humidity: response.data.main.humidity,
     description: response.data.weather[0].description,
-    iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    icon: response.data.weather[0].icon,
 });
 
 
@@ -55,7 +55,7 @@ if(weathwerData.ready){
             </div>
             </div>
             </form>
-            <WeatherInfo data={weathwerData}/>
+            <WeatherInfo data={weathwerData} />
             
         </div>
     )
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+
 
 export default function WeatherInfo (props){
     return (
@@ -14,8 +16,10 @@ export default function WeatherInfo (props){
      <div className="row mt-3">
          <div className="col-6">
              <div className="clearfix">
-                
-             <img src={props.data.iconUrl} alt={props.data.description} className="float-start"/>
+                <div className="float-start">
+                <WeatherIcon code={props.data.icon} />
+                </div>
+            
            <div className="float-start">
             <span className="temperature">{props.data.temperature}</span> <span className="unit">°C </span>
             </div>
